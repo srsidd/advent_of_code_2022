2870a319f34e8862895d4a20b5c9ba9eff05baea
Siddharth Srivatsa <srivatsasiddharth@gmail.com>
2022-12-23 11:10:41 -0500

    Add code for day4 challenge 2

--- src/day4.py
+++ src/day4.py
@@ -43,3 +43,37 @@ for line in data:
         points = points + 1
 
 print("Total number of overlaps: {}".format(points))
+
+# Challenge 2
+f = '../data/day4.txt'
+
+data = read_file(f)
+points = 0
+
+for line in data:
+    sections = line.rstrip().split(",")
+
+    elf1 = sections[0].split("-")
+    elf2 = sections[1].split("-")
+
+    elf1_1 = int(elf1[0])
+    elf1_2 = int(elf1[1])
+
+    elf2_1 = int(elf2[0])
+    elf2_2 = int(elf2[1])
+
+    if elf1_1 >= elf2_1 and elf1_1 <= elf2_2:
+        print("Pair overlap, elf1: {}, elf2: {}".format(elf1, elf2))
+        points = points + 1
+    elif elf1_2 >= elf2_1 and elf1_2 <= elf2_2:
+        print("Pair overlap, elf1: {}, elf2: {}".format(elf1, elf2))
+        points = points + 1
+    elif elf2_1 >= elf1_1 and elf2_1 <= elf1_2:
+        print("Pair overlap, elf1: {}, elf2: {}".format(elf1, elf2))
+        points = points + 1
+    elif elf2_2 >= elf1_1 and elf2_2 <= elf1_2:
+        print("Pair overlap, elf1: {}, elf2: {}".format(elf1, elf2))
+        points = points + 1
+
+
+print("Total number of overlaps: {}".format(points))
